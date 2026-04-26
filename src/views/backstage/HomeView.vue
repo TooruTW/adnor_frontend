db9d30c2dcecb8338a1c56ad59a4fed9fb73043d
Tooru Wu <tom123463@icloud.com>
2026-04-26 12:28:05 +0800

    feat: 在首頁新增編輯按鈕並實作導航功能

--- src/views/backstage/HomeView.vue
+++ src/views/backstage/HomeView.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Button from 'primevue/button'
+
+const router = useRouter()
+
+function goToEdit() {
+  router.push({ name: 'backstage-edit' })
+}
+</script>
 
 <template>
-  <div class="flex-center min-h-screen">
+  <div class="flex-center min-h-screen flex flex-col items-center gap-4">
+    <Button type="button" label="前往編輯頁面" icon="pi pi-plus" @click="goToEdit" />
     <p>this is home view</p>
   </div>
 </template>
